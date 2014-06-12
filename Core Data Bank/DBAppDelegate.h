579ee16cc6b84d757cb394db8f08fa4f0b9c4189
Joshua Howland <development@jkhowland.com>
2014-06-12 09:23:31 -0600

    Initial Commit

---
+++ Core Data Bank/DBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DBAppDelegate.h
+//  Core Data Bank
+//
+//  Created by Joshua Howland on 6/12/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Core Data Bank/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Core Data Bank
+//
+//  Created by Joshua Howland on 6/12/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DBAppDelegate class]));
+    }
+}
